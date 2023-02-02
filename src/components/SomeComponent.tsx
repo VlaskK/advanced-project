ffa714126775fdb3ec9222d3c2dfd4248f194c2e
vlask <vlad.dondokov@gmail.com>
2023-02-02 21:59:42 +0300

    initial commit

---
+++ src/components/SomeComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from "./comp.module.scss"
+
+const SomeComponent = () => {
+    return (
+        <div className={classes.comp}>
+            TEsavasveab
+            <button>
+                Click me
+            </button>
+        </div>
+    );
+};
+
+export default SomeComponent;
