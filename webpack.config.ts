5118cdeffa5c9611f164e9e0e0c95c772cf4c985
vlask <vlad.dondokov@gmail.com>
2023-02-02 23:01:40 +0300

    feature sliced design started

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,3 @@
-import webpack from "webpack";
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 import {BuildEnv, BuildPaths} from "./config/build/types/config";
 import path from "path";
@@ -8,19 +7,18 @@ export default (env: BuildEnv) => {
     const paths: BuildPaths = {
         entry: path.resolve(__dirname, "src", "index.tsx"),
         build: path.resolve(__dirname, "build"),
-        html: path.resolve(__dirname, "public", "index.html")
+        html: path.resolve(__dirname, "public", "index.html"),
+        src: path.resolve(__dirname, "src")
     }
 
     const mode = env.mode || "development";
     const isDev = mode === "development";
     const port = env.port || 3000;
 
-    const config: webpack.Configuration = buildWebpackConfig({
+    return buildWebpackConfig({
         mode: mode,
         paths: paths,
         isDev: isDev,
         port: port,
     })
-
-    return config
 };
