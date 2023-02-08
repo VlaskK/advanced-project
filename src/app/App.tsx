74037d22705fc78f3ed08b10be6754ba26950de7
vlask <vlad.dondokov@gmail.com>
2023-02-08 17:55:57 +0300

    Navbar and first ui component(link)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
 import "./styles/index.scss"
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import AppRouter from "app/providers/router/ui/AppRouter";
+import { Navbar } from 'widgets/Navbar';
 
 
 
@@ -14,15 +13,9 @@ const App = () => {
 
     return (
         <div className={classNames("app", {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
             <button onClick={toggleTheme}>Toggle THEME</button>
-            <Link to={"/"}>Главная</Link>
-            <Link to={"/about"}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={"/about"} element={<AboutPage/>}/>
-                    <Route path={"/"} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
 
         </div>
     );
